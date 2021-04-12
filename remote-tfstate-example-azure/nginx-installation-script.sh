73916136ed75fa197e1b59761dfdcf1274ff7601
Kishan Agarwal <kishanagarwal7668@gmail.com>
2021-04-12 08:08:40 +0000

    remote-state-azure and azure-storage updated

---
+++ remote-tfstate-example-azure/nginx-installation-script.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "Hello I'm going to install java on this machine"
+
+echo "Now going to run sudo apt update command"
+sudo apt update
+
+echo "Installing jdk version 8"
+sudo apt install ngnix
+
+echo "Thanks for executing me operation has been accomplished" > /home/ec2-user/installation.output
